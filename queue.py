546d8fddb99549621268a45dc0e05f55efe38d20
jmwania <josphatmwania237@gmail.com>
2022-01-05 16:32:26 +0300

    Add Queue methods

---
+++ queue.py
@@ -0,0 +1,48 @@
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
+
+class MyStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.max_size = 0
+        self.min_size = 0
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+
+
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        obj.pop(param_2)
+        obj.top(param_3)
+        obj.empty(param_4) 
+
+        print(self.stack)
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        
+
--- reverse_a_string_using_stack.py
+++ reverse_a_string_using_stack.py
@@ -1,57 +1,58 @@
-# # Python program to reverse a string using stack
+# Python program to reverse a string using stack
  
-# # Function to create an empty stack.
-# # It initializes size of stack as 0
-# def createStack():
-#     stack=[]
-#     return stack
+# Function to create an empty stack.
+# It initializes size of stack as 0
+def createStack():
+    stack=[]
+    return stack
  
-# # Function to determine the size of the stack
-# def size(stack):
-#     return len(stack)
+# Function to determine the size of the stack
+def size(stack):
+    return len(stack)
  
-# # Stack is empty if the size is 0
-# def isEmpty(stack):
-#     if size(stack) == 0:
-#         return true
+# Stack is empty if the size is 0
+def isEmpty(stack):
+    if size(stack) == 0:
+        return true
  
-# # Function to add an item to stack .
-# # It increases size by 1
-# def push(stack,item):
-#     stack.append(item)
+# Function to add an item to stack .
+# It increases size by 1
+def push(stack,item):
+    stack.append(item)
  
-# #Function to remove an item from stack.
-# # It decreases size by 1
-# def pop(stack):
-#     if isEmpty(stack): return
-#     return stack.pop()
+#Function to remove an item from stack.
+# It decreases size by 1
+def pop(stack):
+    if isEmpty(stack): return
+    return stack.pop()
  
-# # A stack based function to reverse a string
-# def reverse(string):
-#     n = len(string)
+# A stack based function to reverse a string
+def reverse(string):
+    n = len(string)
      
-#     # Create a empty stack
-#     stack = createStack()
+    # Create a empty stack
+    stack = createStack()
  
-#     # Push all characters of string to stack
-#     for i in range(0,n,1):
-#         push(stack,string[i])
+    # Push all characters of string to stack
+    for i in range(0,n,1):
+        push(stack,string[i])
  
-#     # Making the string empty since all
-#     #characters are saved in stack
-#     string=""
+    # Making the string empty since all
+    #characters are saved in stack
+    string=""
  
-#     # Pop all characters of string and
-#     # put them back to string
-#     for i in range(0,n,1):
-#         string+=pop(stack)
+    # Pop all characters of string and
+    # put them back to string
+    for i in range(0,n,1):
+        string+=pop(stack)
          
-#     return string
+    return string
      
-# # Driver program to test above functions
-# string="GeeksQuiz"
-# string = reverse(string)
-# print("Reversed string is " + string)
+# Driver program to test above functions
+string="GeeksQuiz"
+string = reverse(string)
+print("Reversed string is " + string)
+
  
 
 class Solution(object):
@@ -60,24 +61,29 @@ class Solution(object):
          :type s: str
          :rtype: int
          """
-         stack = []
-         num = 0
-         sign = "+"
-         for c in s:
-            if c.isdigit():
-                 num = num * 10 + int(c)
-            if c in ["+", "-", "*", "/"]:
-                if sign == "+":
-                     stack
-                    elif sign == "-":
-                        stack.append(-num)
-                    elif sign == "*":
-                        stack.append(stack.pop() * num)
-                    elif sign == "/":
-                        stack.append(int(stack.pop() / num))
-                        return sum(stack)
-                    sign = c
-                    num = 0
-            return sum(stack)
+         	
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.inc = Counter() 
+        self.s = []
+        self.maxSize = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.s) < self.maxSize:
+            self.s.append(x)
+
+    def pop(self) -> int:
+        if not self.s:
+            return -1
+        inc = self.inc[len(self.s)]
+        x = self.s[-1]
+        self.inc[len(self.s) - 1] += self.inc[len(self.s)]
+        self.inc[len(self.s)] = 0
+        self.s.pop()
+        return x + inc
+
+    def increment(self, k: int, val: int) -> None:
+        self.inc[min(k, len(self.s))] += val
 
 
